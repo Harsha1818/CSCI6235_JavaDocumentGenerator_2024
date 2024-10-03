36f61e1982c2e806fdf3822fee137451c6d8c730
harsha1818 <harshatumuluri00@gmail.com>
2024-10-02 23:41:46 -0400

    Test files covering inheritence abstraction polymorphism encapsulation. Includes methods constructors interfaces and also method overloading method overriding constructor overloading

---
+++ src/test/java/org/example/TestFolder/OwnerActions.java
@@ -0,0 +1,7 @@
+package org.example.TestFolder;
+
+public interface OwnerActions {
+    void showPetInfo(); // Display information about the pet
+    void feedPet(); // New method to feed the pet
+}
+
